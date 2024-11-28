4070b10f0faab550c42945d4495af3f26548a539
Spyridon Passas <spyridonpassas0325@gmail.com>
2024-11-28 23:27:42 +0100

    Added some smoothing to horizontal movement.

--- Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -10,14 +10,18 @@ namespace lota.gameplay
 	[RequireComponent(typeof(Rigidbody))]
 	public class PlayerController : MonoBehaviour
 	{
+		[Header("Jump Settings")]
 		public float jumpHeightInMeters;
 		public float groundDetectionWidth = 0.1f;
 		public Vector3 groundDetectionPositionOffset;
 		public Vector3 groundDetectionSizeOffset;
 		public LayerMask groundDetectionMask;
 
+		[Header("Horizontal Movement Settings")]
 		public float movementSpeed;
 		public float sprintSpeed;
+		[Range(0.0f, 1.0f)]
+		public float accelerationSpeed = 0.8f;
 
 		private float JumpVelocity => Mathf.Sqrt(2 * Physics.gravity.magnitude * jumpHeightInMeters);
 		private Vector3 GroundDetectorPosition => (collider.bounds.center) - Vector3.up * (collider.bounds.extents.y + (groundDetectionWidth / 2.0f)) + groundDetectionPositionOffset;
@@ -47,6 +51,9 @@ namespace lota.gameplay
 
 			inputService = ServiceLocator<InputService>.Service;
 			mainCamera = Camera.main;
+
+			Cursor.visible = false;
+			Cursor.lockState = CursorLockMode.Locked;
 		}
 		private void Update()
 		{
@@ -62,14 +69,17 @@ namespace lota.gameplay
 
 		private void HandleMovement()
 		{
-			body.linearVelocity += directionalInput * movementSpeed;
+			var newLinearVelocity = directionalInput * movementSpeed;
+			var targetLinearVelocity = new Vector3(newLinearVelocity.x, body.linearVelocity.y, newLinearVelocity.z);
+			var oldLinearVelicity = body.linearVelocity;
+			body.linearVelocity = Vector3.Lerp(oldLinearVelicity, targetLinearVelocity, accelerationSpeed);
 		}
 
 		private void HandleJump()
 		{
 			if (isGrounded && jumpInput && body.linearVelocity.y < 0.5f)
 			{
-				lastKnownYPositionBeforeJump = body.position.y;
+				lastKnownYPositionBeforeJump = collider.bounds.center.y;
 				var targetLinearVelocity = -Physics.gravity.normalized * JumpVelocity;
 				body.linearVelocity += targetLinearVelocity;
 				isGrounded = false;
@@ -96,10 +106,10 @@ namespace lota.gameplay
 
 		private void OnDrawGizmos()
 		{
-			if (!collider)
-			{
-				collider = GetComponent<Collider>();
-			}
+
+			collider ??= GetComponent<Collider>();
+
+
 
 			Gizmos.color = isGrounded ? Color.green : Color.red;
 			Gizmos.DrawCube(GroundDetectorPosition, GroundDetectorSize);
@@ -109,8 +119,8 @@ namespace lota.gameplay
 			Gizmos.DrawWireCube(collider.bounds.center, collider.bounds.size);
 
 			Gizmos.color = Color.cyan;
-			var aPos = transform.position;
-			var bPos = new Vector3(transform.position.x, lastKnownYPositionBeforeJump, transform.position.z) + Vector3.up * jumpHeightInMeters;
+			var aPos = collider.bounds.center;
+			var bPos = new Vector3(collider.bounds.center.x, lastKnownYPositionBeforeJump, collider.bounds.center.z) + Vector3.up * jumpHeightInMeters;
 			Gizmos.DrawSphere(aPos, 0.05f);
 			Gizmos.DrawSphere(bPos, 0.05f);
 			Gizmos.DrawLine(aPos, bPos);
